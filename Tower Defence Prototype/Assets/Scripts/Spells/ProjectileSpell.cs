cbb1dd9ab951173c2666a4988328a4c2a6b47956
Stavros-Kyriacou <stavroslkyriacou@gmail.com>
2021-04-09 17:54:10 +1000

    change to spell and projectile scripts
    moving around the variables from these scripts to be more intuitive

---
+++ Tower Defence Prototype/Assets/Scripts/Spells/ProjectileSpell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileSpell : SpellBase
+{
+    [Header("Projectile")]
+    [SerializeField] private float projectileSpeed;
+    private float destination;
+    private bool reachedDestination;
+
+
+    public float ProjectileSpeed
+    {
+        get
+        {
+            return projectileSpeed;
+        }
+    }
+}
---
+++ Tower Defence Prototype/Assets/Scripts/Spells/SpellBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBase : MonoBehaviour
+{
+    [SerializeField] private float minDamage;
+    [SerializeField] private float maxDamage;
+
+    [SerializeField] private LayerMask targetLayerMask;
+
+
+    [Header("Audio")]
+    [SerializeField] private AudioClip castSFX;
+    [SerializeField] private AudioClip hitSFX;
+    
+
+
+}
